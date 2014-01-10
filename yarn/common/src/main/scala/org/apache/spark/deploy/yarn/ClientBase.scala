3419edb1c8f0d9c93b23ba7ff0d0af2644aed4ab
Raymond Liu <raymond.liu@intel.com>
2014-01-10 09:44:44 +0800

    Address comments to fix code formats

--- yarn/common/src/main/scala/org/apache/spark/deploy/yarn/ClientBase.scala
+++ yarn/common/src/main/scala/org/apache/spark/deploy/yarn/ClientBase.scala
@@ -56,8 +56,6 @@ trait ClientBase extends Logging {
   val args: ClientArguments
   val conf: Configuration
   val sparkConf: SparkConf
-
-  //var rpc: YarnRPC = YarnRPC.create(conf)
   val yarnConf: YarnConfiguration
   val credentials = UserGroupInformation.getCurrentUser().getCredentials()
   private val SPARK_STAGING: String = ".sparkStaging"
@@ -143,9 +141,10 @@ trait ClientBase extends Logging {
     }
     //check for ports
     if (srcUri.getPort() != dstUri.getPort()) {
-      return false
+      false
+    } else {
+      true
     }
-    return true
   }
 
   /** Copy the file into HDFS if needed. */
@@ -172,7 +171,7 @@ trait ClientBase extends Logging {
     destPath
   }
 
-  def qualifyForLocal(localURI : URI): Path = {
+  def qualifyForLocal(localURI: URI): Path = {
     var qualifiedURI = localURI
     // If not specified assume these are in the local filesystem to keep behavior like Hadoop
     if (qualifiedURI.getScheme() == null) {
@@ -300,9 +299,9 @@ trait ClientBase extends Logging {
     retval.toString
   }
 
-  def calculateAMMemory(newApp: GetNewApplicationResponse) :Int
+  def calculateAMMemory(newApp: GetNewApplicationResponse): Int
 
-  def setupSecurityToken(amContainer :ContainerLaunchContext)
+  def setupSecurityToken(amContainer: ContainerLaunchContext)
 
   def createContainerLaunchContext(
         newApp: GetNewApplicationResponse,
